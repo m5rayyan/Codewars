937e17338a030121cdc99e834e89607a5e8065c0
MRayyan <eng.m.rayyan@gmail.com>
2022-12-25 18:47:47 +0200

    feat: Solve Enumerable Magic #4 - True for None

---
+++ 8-kyu/Enumerable Magic #4 - True for None.js
@@ -0,0 +1,5 @@
+/* Create a method none? (JS none) that accepts an array and a block (JS: a function),
+ and returns true if the block (/function) returns true for none of the items in the array.
+ An empty list should return true.*/
+
+const none = (myArray, func) => !myArray.some(func);
