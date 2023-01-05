eb4ffaf643a141ab8c15beef7023584b56f3d82f
MRayyan <eng.m.rayyan@gmail.com>
2023-01-05 20:12:32 +0200

    feat: Beginner Series #3 Sum of Numbers

--- 7-kyu/Beginner Series #3 Sum of Numbers.js
+++ 7-kyu/Beginner Series #3 Sum of Numbers.js
@@ -1,7 +1,19 @@
-// console.log(getSum(0,-1));
-// console.log(getSum(0,1));
-// console.log(getSum(-1,2));
+// Given two integers a and b, which can be positive or negative, find the sum of
+// all the integers between and including them and return it. If the two numbers are
+// equal return a or b.
 
-function getSum(a, b) {
-  //Good luck!
-}
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+const getSum = (a, b) => {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  return ((max - min + 1) * (min + max)) / 2;
+};
