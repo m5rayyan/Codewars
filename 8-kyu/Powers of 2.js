d42a94e1fa828f1a525543ae6bf967de44933ba8
MRayyan <eng.m.rayyan@gmail.com>
2023-01-10 17:23:55 +0200

    feat: Solve Powers of 2

---
+++ 8-kyu/Powers of 2.js
@@ -0,0 +1,11 @@
+// Complete the function that takes a non-negative integer n as input, and returns a
+// list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+const powersOfTwo = (n) => {
+  return Array.from({ length: n + 1 }, (val, n) => Math.pow(2, n));
+};
